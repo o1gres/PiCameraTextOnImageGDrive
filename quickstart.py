d520e4498c38b8082a7ccd87615a9fe716147c4d
Sergio <sergino_89@yahoo.it>
2019-01-10 15:19:18 +0100

    Turn on a ring led RGB to simulate a flash

--- quickstart.py
+++ quickstart.py
@@ -8,6 +8,10 @@ import urllib
 import string
 import json
 import requests
+import time
+from neopixel import *
+import argparse
+
 
 from picamera import PiCamera
 from picamera import Color
@@ -22,9 +26,33 @@ from apiclient.discovery import build
 from apiclient.http import MediaFileUpload
 
 
+#FLASH
+# LED strip configuration:
+LED_COUNT      = 12      # Number of LED pixels.
+LED_PIN        = 18      # GPIO pin connected to the pixels (18 uses PWM!).
+#LED_PIN        = 10      # GPIO pin connected to the pixels (10 uses SPI /dev/spidev0.0).
+LED_FREQ_HZ    = 800000  # LED signal frequency in hertz (usually 800khz)
+LED_DMA        = 10      # DMA channel to use for generating signal (try 10)
+LED_BRIGHTNESS = 70     # Set to 0 for darkest and 255 for brightest
+LED_INVERT     = False   # True to invert the signal (when using NPN transistor level shift)
+LED_CHANNEL    = 0       # set to '1' for GPIOs 13, 19, 41, 45 or 53
+
+
+
 
 # If modifying these scopes, delete the file token.json.
 SCOPES = 'https://www.googleapis.com/auth/drive'
+
+
+
+# Define functions which animate LEDs in various ways.
+def colorWipe(strip, color, wait_ms=50):
+    """Wipe color across display a pixel at a time."""
+    for i in range(strip.numPixels()):
+        strip.setPixelColor(i, color)
+        strip.show()
+        time.sleep(wait_ms/1000.0)
+
 
 def main():
     """Shows basic usage of the Drive v3 API.
@@ -50,10 +78,32 @@ def main():
     data = data.replace(":","-")
 
 
+    #CONFIGURE LED RING
+    # Process arguments
+#    parser = argparse.ArgumentParser()
+#    parser.add_argument('-c', '--clear', action='store_true', help='clear the display on exit')
+#    args = parser.parse_args()
+
+    # Create NeoPixel object with appropriate configuration.
+#    strip = Adafruit_NeoPixel(LED_COUNT, LED_PIN, LED_FREQ_HZ, LED_DMA, LED_INVERT, LED_BRIGHTNESS, LED_CHANNEL)
+    # Intialize the library (must be called once before other functions).
+#    strip.begin()
+
+#    print ('Press Ctrl-C to quit.')
+#    if not args.clear:
+#        print('Use "-c" argument to clear LEDs on exit')
+
+#    try:
+#        colorWipe(strip, Color(255, 255, 255))
+#
+#    except KeyboardInterrupt:
+#        if args.clear:
+#            colorWipe(strip, Color(0,0,0), 10)
+
     camera = PiCamera()
 
     camera.resolution = (2592, 1944)
-
+    camera.brightness = 55
     camera.start_preview()
 
     #Get information from agrumino
@@ -74,8 +124,23 @@ def main():
         print ("error getting json information")
 	pass
 
+
+
+    os.system('python /home/pi/Desktop/flash/rpi_ws281x/python/examples/flashon.py')
+
     camera.capture(applicationPath+'/foto/image' + str(data) + '.jpg')
     camera.stop_preview()
+
+    os.system('python /home/pi/Desktop/flash/rpi_ws281x/python/examples/flashoff.py')
+
+
+ #   try:
+ #       colorWipe(strip, Color(0, 0, 0))
+#
+#    except KeyboardInterrupt:
+#        if args.clear:
+#            colorWipe(strip, Color(0,0,0), 10)
+
 
 
     #upload to drive
